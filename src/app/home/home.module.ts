f2a8be7c4f05f3512ff08ec9151e19110ef56711
alpag <pawlaq123@gmail.com>
2019-02-27 23:55:45 +0100

    New RWD bootstrap header

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -4,22 +4,21 @@ import { CommonModule } from '@angular/common';
 import { LandingPageComponent } from './landing-page/landing-page.component';
 import { NotFoundComponent } from './not-found/not-found.component';
 import { HomeHeaderComponent } from './home-header/home-header.component';
+import { HomeHeaderNavbarComponent } from './home-header/home-header-navbar.component';
 import { HomeFooterComponent } from './home-footer/home-footer.component';
 
-import {MatToolbarModule} from '@angular/material/toolbar';
-import {MatButtonModule} from '@angular/material/button';
+
 
 @NgModule({
-  declarations: [LandingPageComponent, NotFoundComponent, HomeHeaderComponent, HomeFooterComponent],
+  declarations: [LandingPageComponent, NotFoundComponent, HomeHeaderComponent, HomeHeaderNavbarComponent, HomeFooterComponent],
   imports: [
-    CommonModule,
-    MatToolbarModule,
-    MatButtonModule
+    CommonModule
   ],
   exports: [
     LandingPageComponent,
     NotFoundComponent,
     HomeHeaderComponent,
+    HomeHeaderNavbarComponent,
     HomeFooterComponent
   ]
 })
