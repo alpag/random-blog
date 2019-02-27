4e9eaa60f7c261f1bc045bc8d3f6c538eea6ce5b
alpag <pawlaq123@gmail.com>
2019-02-27 21:19:34 +0100

    Basic components, bootstrap, loader and mixins

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -1,16 +1,26 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+
 import { LandingPageComponent } from './landing-page/landing-page.component';
 import { NotFoundComponent } from './not-found/not-found.component';
+import { HomeHeaderComponent } from './home-header/home-header.component';
+import { HomeFooterComponent } from './home-footer/home-footer.component';
+
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatButtonModule} from '@angular/material/button';
 
 @NgModule({
-  declarations: [LandingPageComponent, NotFoundComponent],
+  declarations: [LandingPageComponent, NotFoundComponent, HomeHeaderComponent, HomeFooterComponent],
   imports: [
-    CommonModule
+    CommonModule,
+    MatToolbarModule,
+    MatButtonModule
   ],
   exports: [
     LandingPageComponent,
-    NotFoundComponent
+    NotFoundComponent,
+    HomeHeaderComponent,
+    HomeFooterComponent
   ]
 })
 export class HomeModule { }
