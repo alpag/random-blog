16c4d3a2d432875367056265bbe8f776db4c2a4a
alpag <pawlaq123@gmail.com>
2019-03-04 16:56:08 +0100

    Rebuild of app.component, head element of landing page

--- src/app/home/home-header/home-header-navbar.component.ts
+++ src/app/home/home-header/home-header-navbar.component.ts
@@ -4,12 +4,15 @@ import { Component, OnInit } from '@angular/core';
     selector: 'home-header-navbar',
     template: `
     <nav class="collapse navbar-collapse" id="navbarSupportedContent">
-      <ul class="navbar-nav mr-auto">
+      <ul class="navbar-nav ml-auto">
          <li class="nav-item active">
             <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
          </li>
          <li class="nav-item">
             <a class="nav-link" href="#">Articles</a>
+         </li>
+         <li class="nav-item">
+            <a class="nav-link" href="#">Currencies</a>
          </li>
          <li class="nav-item">
             <a class="nav-link" href="#">Contact</a>
