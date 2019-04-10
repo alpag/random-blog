cad998d92cd535e0d54c5afa6fe893aecab2c0ac
alpag <pawlaq123@gmail.com>
2019-04-10 14:45:13 +0200

    Debounce of email input, doubled

--- src/app/contact/contact-form/contact-form.component.ts
+++ src/app/contact/contact-form/contact-form.component.ts
@@ -1,6 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { ContactService } from 'src/app/core';
+import { Subject, Subscription } from 'rxjs';
 import { ContactMessage } from 'src/app/shared/models/contact-message.model';
+
+import { debounceTime, distinctUntilChanged } from 'rxjs/operators';
 
 @Component({
   selector: 'app-contact-form',
@@ -9,15 +12,24 @@ import { ContactMessage } from 'src/app/shared/models/contact-message.model';
 })
 export class ContactFormComponent implements OnInit {
   message : ContactMessage = new ContactMessage();
+  private onChange : Subject<string> = new Subject<string>();
+
   submitted = false;
-  constructor(private contactService : ContactService) { }
+  constructor(private contactService : ContactService) {
+    this.onChange.pipe(
+      debounceTime(1000), 
+      distinctUntilChanged())
+      .subscribe(mail => this.message.authorEmail = mail);
+   }
 
   ngOnInit() {
+    
   }
 
   onSubmit(){
     this.contactService.contactAction(this.message);
     this.submitted = true;
   }
+
 
 }
